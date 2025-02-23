4f8edc0bd10bb77b8bb95f1d06248729c22bb4fc
joshuavansynghel <120260688+joshuavansynghel@users.noreply.github.com>
2025-02-23 20:28:39 +0100

    style project

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,7 @@ app_name = 'users'
 urlpatterns = [
 	# Include default auth urls.
 	path('', include('django.contrib.auth.urls')),
+
+	# Registration page.
+	path('register/', views.register, name='register'),
 ]
